9daf9f8e0195fe394d91f938126094e859fc1eda
Jorengarenar <Jorengarenar@users.noreply.github.com>
2020-11-01 13:55:56 +0000

    deploy: 53bae68c3683708d1a6a55a8620faa5b2638e692

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['blank',['blank',['../structWindow_1_1Opts.html#a3146863f9b3e1fe3812d4e7f7727982d',1,'Window::Opts']]],
-  ['boost_5fauto_5ftest_5fcase',['BOOST_AUTO_TEST_CASE',['../test__buffer_8cpp.html#a6e6a3af2f4a8a1c13a46628c61ed10a9',1,'BOOST_AUTO_TEST_CASE(bar):&#160;test_buffer.cpp'],['../test__buffer_8cpp.html#a8cd2d305ff695cc7c85f12e72f5765b9',1,'BOOST_AUTO_TEST_CASE(size):&#160;test_buffer.cpp'],['../test__buffer_8cpp.html#a2f96265464dc087ceabfd7abbffdfd45',1,'BOOST_AUTO_TEST_CASE(empty):&#160;test_buffer.cpp']]],
+  ['boost_5fauto_5ftest_5fcase',['BOOST_AUTO_TEST_CASE',['../test__buffer_8cpp.html#a8cd2d305ff695cc7c85f12e72f5765b9',1,'BOOST_AUTO_TEST_CASE(size):&#160;test_buffer.cpp'],['../test__buffer_8cpp.html#a2f96265464dc087ceabfd7abbffdfd45',1,'BOOST_AUTO_TEST_CASE(empty):&#160;test_buffer.cpp']]],
   ['boost_5ftest_5fmodule',['BOOST_TEST_MODULE',['../test__buffer_8cpp.html#a6b2a3852db8bb19ab6909bac01859985',1,'test_buffer.cpp']]],
   ['buf',['buf',['../classWindow.html#ac098fc6f9173b86405c4b2ee28947edc',1,'Window']]],
   ['buffer',['Buffer',['../classBuffer.html',1,'Buffer'],['../classWindow.html#aba8f47da0ace15437608fc211e4a999d',1,'Window::buffer()'],['../classBuffer.html#ae7ef2cd201190fde551dcb902627112b',1,'Buffer::Buffer()'],['../classBuffer.html#a564a5d0c13f5458a5c0d0056331c2ca6',1,'Buffer::Buffer(const char *)']]],
